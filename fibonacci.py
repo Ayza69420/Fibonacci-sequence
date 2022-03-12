d56f7f2d9b95882be65928351850d10390146085
Ayza <87999011+Ayza69420@users.noreply.github.com>
2022-03-12 04:56:59 +0300

    Create fibonacci.py

---
+++ fibonacci.py
@@ -0,0 +1,15 @@
+def fibonacci(end):
+    x = 0
+    n = 1
+
+    sequence = [1]
+
+    for _ in range(end-1):
+        sequence.append(x + n)
+
+        n,x = sequence[-1], sequence[-2] # n should be equal to the current sum, and x being the one preceding it
+
+    return sequence
+
+
+print(fibonacci(10)) # Should return and print the first 10 fibonacci numbers
